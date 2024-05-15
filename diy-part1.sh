46610d26f74ad609bda62b4887eba9e474c1a81d
ASDHiope <62837496+ASDHiope@users.noreply.github.com>
2024-05-15 09:19:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/Asunioo/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/Asunioo/small' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git node https://github.com/nxhack/openwrt-node-packages.git' >>feeds.conf.default
